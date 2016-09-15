a7452b7d76d578b285372ce436342525769e5311
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-09-15 16:56:53 -0600

    Added a default program template from Material Icons Lite.

---
+++ src/client/js/root.js
@@ -0,0 +1,6 @@
+var Test = require('./test.js');
+
+$(document).ready(function() {
+  var test = new Test();
+  test.testFunction();
+});
---
+++ src/client/js/test.js
@@ -0,0 +1,13 @@
+
+var Test = function () {
+
+  // Public Function
+  this.testFunction = function() {
+    privateTest('This is a test.');
+  };
+
+  // Private Function
+  var privateTest = function (value) {
+    alert(value);
+  }
+}
