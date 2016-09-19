9703a4826a8961666c4e5a3509c7454df5c28f93
Jared Rewerts <jared.rewerts@edmonton.ca>
2016-09-19 10:06:22 -0600

    Fixed linting issues.

--- src/client/js/test.js
+++ src/client/js/test.js
@@ -1,15 +1,35 @@
 
-var Test = function () {
 
-  // Public Function
+
+/**
+ * This object just acts as an example for how to include files using browserify.
+ *
+ * @constructor
+ */
+var Test = function() {
+
+  /**
+   * This is just an example of how to use private functions inside public functions.
+   *
+   * @public
+   */
   this.testFunction = function() {
     privateTest('This is a test.');
   };
 
-  // Private Function
-  var privateTest = function (value) {
+  /**
+   * This is an example of a private function. It can only be called inside this class.
+   *
+   * @param {string} value Print this in an alert message box.
+   * @private
+   */
+  var privateTest = function(value) {
     alert(value);
-  }
-}
+  };
+};
 
+
+/**
+ * Ignore this.
+ */
 module.exports = Test;
